a5873ab2e39c47485ba341c5b3ec0730f77e8d69
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2024-01-09 15:04:11 -0300

    Add files via upload

---
+++ script/ConvertPDF.js
@@ -0,0 +1,15 @@
+/*
+\\Requisitos para o script\\
+
+Carregar pdf na máquina
+alterar nome do arquivo
+alterar tamanho e qualidade (Em pixels)
+escolher quantas páginas serão impressas
+escolher colorido ou preto e branco
+*/
+
+//Criar função para importar elementos e funções de outros scripts
+
+import PDFjsLibrary from "./modules/jspdf";
+
+PDFjsLibrary
